c7fb55751f73602cdc28689f4497a1a4a4416047
Minh Do <minhdb@bytesoft.net>
2020-04-01 10:50:55 +0700

    AppError

--- controllers/auth.js
+++ controllers/auth.js
@@ -5,7 +5,7 @@ exports.signup = async (req, res, next) => {
   try {
     const user = await authService.signup(req.body);
 
-    return res.json({ success: true, user }).status(200);
+    return res.status(200).json({ success: true, user });
   } catch (error) {
     Logger.error('🔥 error: %o', error.message);
     return next(error);
